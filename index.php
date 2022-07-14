738130e5b445456a39cad026c850b66173e381ad
enisgjini <gjinienis148@gmail.com>
2022-07-14 20:11:39 +0200

    Edit accordion and change icon's

--- index.php
+++ index.php
@@ -52,10 +52,7 @@
                     I know to the next generation of designers.‍</p>
             </div>
             <div class="col">
-
-                <p>In this example we have added a "plus" sign to each button. When the user clicks on the button, the
-                    "plus" sign is replaced with a "minus" sign.</p>
-                <button class="accordion">Section 1</button>
+                <button class="accordion">Branding/Logo</button>
                 <div class="panel">
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
