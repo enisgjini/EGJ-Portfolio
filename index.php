535292ba7f79010020b45de7d3f2b76f4b9f933a
enisgjini <gjinienis148@gmail.com>
2022-07-15 16:49:33 +0200

    #

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <!-- Here are meta tag's -->
-    <meta charset="utf-8">
+    <meta charset="utf-8">#
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- <meta http-equiv="Content-Security-Policy" content="script-src-elem 'none'"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
